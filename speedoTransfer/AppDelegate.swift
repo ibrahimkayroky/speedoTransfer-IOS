f3d4c8d1ba830046812e3f7ad2e207f295b887c7
Karim Ahmed <karim2elsaid@gmail.com>
2024-09-09 00:16:17 +0300

    This is the signup and signin.
    This is the signup and signin pages that will redirect you to the home page.

--- speedoTransfer/AppDelegate.swift
+++ speedoTransfer/AppDelegate.swift
@@ -1,36 +1,24 @@
-//
-//  AppDelegate.swift
-//  speedoTransfer
-//
-//  Created by 1234 on 30/08/2024.
-//
-
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//        // Initialize the window
+//        window = UIWindow(frame: UIScreen.main.bounds)
+//        
+//        // Load the main storyboard
+//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//        
+//        // Instantiate the initial view controller from the storyboard
+//        let initialViewController = storyboard.instantiateViewController(withIdentifier: "FirstSignUpViewController")
+//        
+//        // Set the root view controller and make the window visible
+//        window?.rootViewController = initialViewController
+//        window?.makeKeyAndVisible()
+//        
+//        return true
+//    }
 }
-
